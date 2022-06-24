921a18b2ee1a35656366807ba901f838976d5933
Martin <samardzievm@gmail.com>
2022-06-24 17:00:12 +0200

    Initial commit

---
+++ _nuxt/32de499.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{285:function(t,n,e){"use strict";e.r(n);var o={name:"ContainerComponent"},c=e(20),component=Object(c.a)(o,(function(){var t=this,n=t.$createElement;return(t._self._c||n)("div",{staticClass:"container mx-auto mt-16 mb-16"},[t._t("default")],2)}),[],!1,null,"2169dc6d",null);n.default=component.exports}}]);
